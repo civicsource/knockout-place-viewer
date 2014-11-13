d41f199c71b562f3b8d46de251bf27e31d87978b
Brian Oline <brian.oline@archoninfosys.com>
2014-11-13 15:32:46 -0600

    CS-12317: adding new place viewer component to show readonly version of fips location

---
+++ main.js
@@ -0,0 +1,22 @@
+﻿define(["knockout", "jquery", "app/urls"],
+function (ko, $, urls) {
+	ko.bindingHandlers.placeViewer = {
+		update: function (el, valueAccessor, allBindings) {
+			el.innerHtml = "<span>loading...</span>";
+			var locationName = "";
+			var fips = ko.unwrapObservable(valueAccessor) || "";
+
+			$.ajax(urls.fips + "places/" + fips, {
+				type: "GET",
+				contentType: "application/json",
+			}).then(function (data) {
+				locationName = data.fullName;
+			}).always(function () {
+				if (!locationName) locationName = "Unknown";
+
+				el.innerHtml = "<span>" + locationName + "</span>";
+			});
+		}
+	};
+	ko.virtualElements.allowedBindings.placeViewer = true;
+});
