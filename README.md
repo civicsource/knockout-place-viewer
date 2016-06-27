0f68db290547318712a0b9ed03f63f8455000386
Mark D'Arensbourg <mdarens@gmail.com>
2016-06-27 15:27:20 -0500

    Make consumable via npm (#1)
    * make consumable via npm
    * Make endpoint configurable

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 > Knockout component to view a [FIPs place](https://github.com/civicsource/fips).
 
-##Install with [Bower](http://bower.io/)
+##Installation
 
 ```
-bower install civicsource/knockout-place-viewer --save
+npm install knockout-place-viewer --save
 ```
 
 Then add the folder to your project.
@@ -21,5 +21,7 @@ Then add the folder to your project.
 where `myPlace` is an observable you want two-way bound to the component.
 
 If the bound place only has a `fips` code, it will hit the server to retrieve the full place object and populate the observable. Otherwise, it will use what is passed in to show the place's full name.
+
+This component will look for a payloaded endpoint URL on the window object by default. To configure this, pass the `endpoint` parameter to the binding.
 
 This component complements and can be used with the [Knockout Place Picker](https://github.com/civicsource/knockout-place-picker)
--- main.js
+++ main.js
@@ -1,27 +1,29 @@
-﻿define(["knockout", "jquery", "app/payload"], function (ko, $, payload) {
-	ko.bindingHandlers.placeViewer = {
-		update: function (el, valueAccessor, allBindings) {
-			var place = ko.utils.unwrapObservable(valueAccessor());
+﻿var ko = require("knockout");
+var $ = require("jquery");
 
-			if (!place) {
-				el.innerHTML = "No place provided";
+ko.bindingHandlers.placeViewer = {
+	update: function (el, valueAccessor, allBindings) {
+		var place = ko.utils.unwrapObservable(valueAccessor());
+		var endpoint = allBindings.get("endpoint") || window.civicsource.urls.fips + "places/";
+
+		if (!place) {
+			el.innerHTML = "No place provided";
+		} else {
+			if (place.fullName) {
+				el.innerHTML = place.fullName;
 			} else {
-				if (place.fullName) {
-					el.innerHTML = place.fullName;
-				} else {
-					$.ajax(payload.urls.fips + "places/" + place.fips + "/", {
-						type: "GET",
-						contentType: "application/json",
-					}).then(function (data) {
-						var obs = valueAccessor();
-						if (ko.isObservable(obs)) {
-							obs(data);
-						}
+				$.ajax(endpoint + place.fips + "/", {
+					type: "GET",
+					contentType: "application/json"
+				}).then(function (data) {
+					var obs = valueAccessor();
+					if (ko.isObservable(obs)) {
+						obs(data);
+					}
 
-						el.innerHTML = data.fullName;
-					});
-				}
+					el.innerHTML = data.fullName;
+				});
 			}
 		}
-	};
-});
+	}
+};
